36eec729d7febb66118c601ce57eb39720a196e8
shreyarainakaul <118874731+shreyarainakaul@users.noreply.github.com>
2023-08-14 16:09:12 +1000

    Add files via upload
    3. Write queries to return the following: 
    Find out the number of cities in each state.
    
    Find out the number of unique postcodes, and suburbs in each city .
    
    Query the Average Property Median Value by Suburb, and by Postcode separately, and then by Suburb and Postcode together. Then put Where condition to remove those records where there is no median value.

---
+++ OnboardingTask2023.sql
@@ -0,0 +1,40 @@
+/* 3(a).Find out the number of cities in each state.
+
+SELECT state,COUNT(city) AS NoofCities
+FROM ['AUS_Post_suburb v2$']
+Group By state
+
+
+
+3(b).Find out the number of unique postcodes, and suburbs in each city .
+
+SELECT DISTINCT city,COUNT(postcode) AS NOofPostcodes,COUNT(suburb) AS Noofsuburb
+FROM ['AUS_Post_suburb v2$']
+GROUP BY city,state
+
+
+
+3(c) Query the Average Property Median Value by Suburb
+
+SELECT suburb,AVG(Property_Median_Value) AS AveragePropertyMedianValue
+FROM NSW_PropertyMedainValue#csv$
+Group By Suburb
+
+3(c) Query the Average Property Median Value by Postcode 
+
+SELECT Postcode,AVG(Property_Median_Value) AS AveragePropertyMedianValue
+FROM NSW_PropertyMedainValue#csv$
+Group By Postcode
+
+3(c)Query the Average Property Median Value by Suburb and Postcode together
+
+SELECT suburb,Postcode,AVG(Property_Median_Value) AS AveragePropertyMedianValue
+FROM NSW_PropertyMedainValue#csv$
+Group By Suburb,Postcode
+
+3(c)put Where condition to remove those records where there is no median value.
+
+SELECT suburb,Postcode,AVG(Property_Median_Value) AS AveragePropertyMedianValue
+FROM NSW_PropertyMedainValue#csv$
+Where Property_Median_Value IS NOT NULL
+Group By Suburb,Postcode
